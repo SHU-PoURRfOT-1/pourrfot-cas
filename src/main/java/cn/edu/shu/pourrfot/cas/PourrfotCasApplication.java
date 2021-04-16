0cff6ca5a11bb651d7cc712bc887ac2ac83c9844
spencercjh <shouspencercjh@foxmail.com>
2021-04-16 10:36:43 +0800

    init Spring Boot

---
+++ src/main/java/cn/edu/shu/pourrfot/cas/PourrfotCasApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.pourrfot.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PourrfotCasApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PourrfotCasApplication.class, args);
+  }
+
+}
---
+++ src/test/java/cn/edu/shu/pourrfot/cas/PourrfotCasApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.shu.pourrfot.cas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PourrfotCasApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
