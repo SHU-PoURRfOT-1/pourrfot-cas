523efdaba7fbc2e3ff85caec566ebc9a031a8184
spencercjh <shouspencercjh@foxmail.com>
2021-05-12 02:35:02 +0800

    Add docs

--- src/main/java/cn/edu/shu/pourrfot/cas/model/dto/OauthAuthorizationCodeRequest.java
+++ src/main/java/cn/edu/shu/pourrfot/cas/model/dto/OauthAuthorizationCodeRequest.java
@@ -1,6 +1,8 @@
 package cn.edu.shu.pourrfot.cas.model.dto;
 
 import cn.edu.shu.pourrfot.cas.enums.RoleEnum;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,23 +15,31 @@ import java.net.URL;
 /**
  * @author spencercjh
  */
+@ApiModel(value = "cn-edu-shu-pourrfot-server-model-dto-OauthAuthorizationCodeRequest")
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OauthAuthorizationCodeRequest {
+  @ApiModelProperty(required = true)
   @NotBlank
   private String username;
+  @ApiModelProperty(required = true)
   @NotBlank
   private String password;
+  @ApiModelProperty(example = "http://pourrfot-server.com/api", required = true)
   @NotNull
   private URL redirectUrl;
+  @ApiModelProperty(example = "code", required = true)
   @NotBlank
   private String responseType;
+  @ApiModelProperty(example = "pourrfot-web", required = true)
   @NotBlank
   private String clientId;
+  @ApiModelProperty(required = true)
   @NotNull
   private RoleEnum scope;
+  @ApiModelProperty(example = "state", required = true)
   @NotBlank
   private String state;
 }
--- src/main/java/cn/edu/shu/pourrfot/cas/model/dto/OauthTokenResponse.java
+++ src/main/java/cn/edu/shu/pourrfot/cas/model/dto/OauthTokenResponse.java
@@ -1,6 +1,8 @@
 package cn.edu.shu.pourrfot.cas.model.dto;
 
 import cn.edu.shu.pourrfot.cas.model.PourrfotUser;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,13 +11,18 @@ import lombok.NoArgsConstructor;
 /**
  * @author spencercjh
  */
+@ApiModel(value = "cn-edu-shu-pourrfot-server-model-dto-OauthTokenResponse")
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OauthTokenResponse {
+  @ApiModelProperty()
   private String token;
+  @ApiModelProperty()
   private long expireAt;
+  @ApiModelProperty()
   private PourrfotUser user;
+  @ApiModelProperty()
   private String message;
 }
