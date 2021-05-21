7a8a2ec9ee0fe384d73dde3b7416be38e1d0eca2
spencercjh <shouspencercjh@foxmail.com>
2021-05-22 00:09:47 +0800

    Use swagger 2

--- src/main/java/cn/edu/shu/pourrfot/cas/config/SwaggerUiWebMvcConfigurer.java
+++ src/main/java/cn/edu/shu/pourrfot/cas/config/SwaggerUiWebMvcConfigurer.java
@@ -1,12 +1,17 @@
 package cn.edu.shu.pourrfot.cas.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author spencercjh
@@ -17,6 +22,15 @@ public class SwaggerUiWebMvcConfigurer implements WebMvcConfigurer {
 
   public SwaggerUiWebMvcConfigurer(@Value("${springfox.documentation.swagger-ui.base-url:}") String baseUrl) {
     this.baseUrl = baseUrl;
+  }
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+      .select()
+      .apis(RequestHandlerSelectors.any())
+      .paths(PathSelectors.any())
+      .build();
   }
 
   @Override
@@ -35,8 +49,6 @@ public class SwaggerUiWebMvcConfigurer implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry
-      .addMapping("/cas/api")
-      .allowedOrigins("https://shu-pourrfot-1.github.io/pourrfot-web");
+    registry.addMapping("/cas/api");
   }
 }
